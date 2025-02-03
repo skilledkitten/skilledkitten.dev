27194ba7b79dfb1d3e4b03c4a7fcdde453b3b7e3
skilledkitten <147549908+skilledkitten@users.noreply.github.com>
2025-02-03 14:16:24 +0100

    Update color-generator.tsx

--- components/color-generator.tsx
+++ components/color-generator.tsx
@@ -33,6 +33,7 @@ function hslToRgb(h: number, s: number, l: number): string {
   l /= 100;
   const k = (n: number) => (n + h / 30) % 12;
   const a = s * Math.min(l, 1 - l);
+  const f = (n: number) =>
     l - a * Math.max(Math.min(k(n) - 3, 9 - k(n), 1), -1);
   return `rgb(${Math.round(f(0) * 255)}, ${Math.round(
     f(8) * 255
