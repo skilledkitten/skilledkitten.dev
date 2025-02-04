5114e909f4e2c920661a8f4b7e895ba037fc25bf
skilledkitten <147549908+skilledkitten@users.noreply.github.com>
2025-02-04 13:14:36 +0100

    whyy plase workkkk

---
+++ app/tools/page.tsx
@@ -0,0 +1,9 @@
+import { ToolsGrid } from '../../components/tools/tools-grid';
+
+export default function ToolsPage() {
+  return (
+    <div className="min-h-screen pt-20">
+      <ToolsGrid />
+    </div>
+  );
+}
