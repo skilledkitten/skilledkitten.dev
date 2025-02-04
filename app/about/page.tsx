ebaca8e153c48658cb72e898f88bed871ac43f12
skilledkitten <147549908+skilledkitten@users.noreply.github.com>
2025-02-04 13:34:47 +0100

    Pleaseee

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+
+"use client";
+
+import { About } from "@/components/about";
+
+export default function AboutPage() {
+  return <About isActive={true} onExpand={() => {}} />;
+}
---
+++ app/projects/page.tsx
@@ -0,0 +1,8 @@
+
+'use client';
+
+import { Projects } from '@/components/projects';
+
+export default function ProjectsPage() {
+  return <Projects />;
+}
